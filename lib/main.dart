a311612fab29d559d135227da237c7be31485ddf
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-03 23:17:29 +0900

    新しいパッケージを追加した

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,18 +7,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        appBar: AppBar(
-          title:Text('Welcom to Flutter!'),
-        ),
-        body: Center(
-          child:Text('Hello World!')
-        )
-      ),
+          appBar: AppBar(
+            title: Text('Welcom to Flutter!'),
+          ),
+          body: Center(child: Text(wordPair.asPascalCase))),
     );
   }
 }
