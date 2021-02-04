adc16f3845dd61034f27212decdc0e7a0b4ae569
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-04 23:16:37 +0900

    ランダム生成した名前をリスト表示するようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
+ 
+void main() => runApp(MyApp());
+ 
 class RandomWordsState extends State<RandomWords> {
+  
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 16.0);
+  
   @override
-  Widget build(BuildContext build) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+      );
+  }
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2; // int型へ
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
-
+ 
 class RandomWords extends StatefulWidget {
   @override
-  RandomWordsState createState() => new RandomWordsState();
+  RandomWordsState createState() => RandomWordsState();
 }
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  final wordPair = WordPair.random();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcom to Flutter!'),
-          ),
-          body: Center(child: RandomWords())),
+      home: RandomWords(),
     );
   }
 }
